7bdd3efbf01af4bd104962aac4762fca954dc4fd
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-06-26 15:53:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins on EC2!"
+    return "Hello from Jenkins on EC2! hi"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
